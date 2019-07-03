64c342318e94574e47b0f03702b776a42c7b9c15
JPantana <joshpantana@me.com>
2019-07-02 21:19:57 -0500

    auth works

---
+++ src/components/MyNavbar/MyNavbar.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { NavLink as RRNavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+import PropTypes from 'prop-types';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool,
+  }
+
+  state = {
+    isOpen: false,
+  }
+
+  toggle() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  };
+
+
+  render() {
+    const { authed } = this.props;
+    const buildNavbar = () => {
+      if (authed) {
+        return (
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink tag={RRNavLink} to="/home">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RRNavLink} to="/new">New Crap</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink onClick={this.logMeOut} href="https://github.com/reactstrap/reactstrap">Logout</NavLink>
+          </NavItem>>
+        </Nav>
+        );
+      }
+      return <nav className="ml-auto" />;
+    };
+
+    return (
+      <div className="MyNavbar">
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Hoarder</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            {buildNavbar()}
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
